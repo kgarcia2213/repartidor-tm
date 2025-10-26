b2e881223d815d72432c6d9b33650fecd087f830
kgarcia2213 <kenia.garcia2213@gmail.com>
2025-10-26 16:12:28 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ app.use(
   })
 );
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 
 // ---------------------- LOGIN ----------------------
